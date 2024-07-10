70949804f5bbbab9ae0e78592879e9b33910df3c
Ravi Gautam <ravi.gautam@tietoevry.com>
2024-07-10 19:00:03 +0530

    commited code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-github-actions'`, () => {
+  it(`should have as title 'angular-github-actions validate'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-github-actions');
